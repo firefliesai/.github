2928d5f7e5b0637aba7b256ead687f95731f6b4b
Hoang Vo <dev+g@hvvo.dev>
2024-01-27 15:32:06 +0700

    gitflow-release: fix versioning and add auto commit package json version

--- .github/scripts/gitflow-release.js
+++ .github/scripts/gitflow-release.js
@@ -1,6 +1,6 @@
 // @ts-check
 /** @param {import('github-script').AsyncFunctionArguments & { pull_number: number, pull_numbers_in_release: string, latest_release_tag_name?: string }} AsyncFunctionArguments */
-export default async ({ core, context, github, pull_numbers_in_release, latest_release_tag_name }) => {
+export default async ({ core, context, github, pull_numbers_in_release }) => {
   try {
     const mergedPrNumbers = Array.from(new Set(pull_numbers_in_release.split(',').map(Number)));
     
@@ -11,10 +11,7 @@ export default async ({ core, context, github, pull_numbers_in_release, latest_r
         repo: context.repo.repo,
         pull_number: prNumber
       });
-      const body = pr.data.body;
-      if (!body) {
-        return;
-      }
+      const body = pr.data.body || "";
       const regex = /\#\# What does this PR do\?([\s\S]*?)\n\#\#/gm;
       let match = regex.exec(body)?.[1]?.trim();
       
@@ -39,21 +36,16 @@ export default async ({ core, context, github, pull_numbers_in_release, latest_r
       return `${pr.title}\n${pr.summary}`;
     }).join('\n\n');
 
-    let version = latest_release_tag_name || "0.0.0";
-    const bumpType = Math.max(...mergedPrs.map((pr) => pr.type));
-    let [major, minor, patch] = version.split('.').map(Number);
-    if (bumpType === 2) major += 1;
-    else if (bumpType === 1) minor += 1;
-    else patch += 1;
-    version = `${major}.${minor}.${patch}`;
+    const versionIncrementType = Math.max(...mergedPrs.map((pr) => pr.type));
+    const versionIncrement = ['patch', 'minor', 'major'][versionIncrementType] || 'patch';;
 
-    console.log(`Determined suggested version: ${version} (${['patch', 'minor', 'major'][bumpType]})`)
+    console.log(`Determined suggested version increment (${versionIncrement})`)
     console.log(`Release summary:\n${releaseSummary}`);
 
     core.setOutput('release_summary', releaseSummary);
-    core.setOutput('version', version);
+    core.setOutput('version_increment', versionIncrement);
   } catch (error) {
-    console.log(`Could not determine release summary and version`)
+    console.log(`Could not determine release summary and version_increment`)
     console.log(error)
   }
 };
