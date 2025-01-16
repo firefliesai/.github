48bd25664b2603e7291b9b233b20ed176f1a4789
Lucas Matheus <lucasrobertomatheus@gmail.com>
2025-01-16 14:48:19 -0300

    fix: update warning and error messages for non-fixed alerts in quality gate script

--- .github/scripts/quality_gate.sh
+++ .github/scripts/quality_gate.sh
@@ -21,7 +21,14 @@ if [ $alertCount -gt 0 ]; then
 
     non_fixed_count=$(jq '[.[] | select(.fixed_at == null)] | length' "$getFile")
     echo "Number of non-fixed alerts: $non_fixed_count"
-
+    
+    if [ "$non_fixed_count" -gt 5 ] && [ "$non_fixed_count" -le 10 ]; then
+        echo "\033[33mWARNING: There are more than 5 non-fixed alerts in the repository. Please verify the alerts on the below links.\033[0m"
+        echo "Links to all alerts:"
+        jq -r '.[] | .html_url' "$getFile"
+        echo "WARNING: There are more than 5 non-fixed alerts in the repository. Please verify the alerts on the below links." >> $GITHUB_STEP_SUMMARY
+        jq -r '.[] | .html_url' "$getFile" >> $GITHUB_STEP_SUMMARY
+    fi
 
     if [ "$non_fixed_count" -gt 10 ]; then
         echo "\033[31mERROR: There are more than 10 non-fixed alerts in the repository. Please verify the alerts on the below links. Exiting...\033[0m"
@@ -30,15 +37,6 @@ if [ $alertCount -gt 0 ]; then
         echo "ERROR: There are more than 10 non-fixed alerts in the repository. Please verify the alerts on the below links." >> $GITHUB_STEP_SUMMARY
         jq -r '.[] | .html_url' "$getFile" >> $GITHUB_STEP_SUMMARY
         exit 1
-    fi
-
-
-    if [ "$non_fixed_count" -gt 5 ] && [ "$non_fixed_count" -le 10 ]; then
-        echo "\033[33mWARNING: There are more than 5 non-fixed alerts in the repository. Please verify the alerts on the below links.\033[0m"
-        echo "Links to all alerts:"
-        jq -r '.[] | .html_url' "$getFile"
-        echo "WARNING: There are more than 5 non-fixed alerts in the repository. Please verify the alerts on the below links." >> $GITHUB_STEP_SUMMARY
-        jq -r '.[] | .html_url' "$getFile" >> $GITHUB_STEP_SUMMARY
     fi
 
 else
